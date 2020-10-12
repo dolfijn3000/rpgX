9fc97bb6553e202d532cc0fa9291f545e9da1123
dolfijn3000 <yente.paternotte@hotmail.com>
2020-10-12 07:04:27 +0200

    sync 12/10/2020

---
+++ src/components/Headerbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Headerbar extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <li>
+                    test
+                </li>
+            </div>
+        );
+    }
+}
+
+export default Headerbar
